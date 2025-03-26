88c2540493477314c167e70c70e1c0a6b85f8f9b
mamali1009 <mohammed.abdulmueedali@cognizant.com>
2025-03-26 17:52:51 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,17 @@
-# lambda_function.py
 import aws_lambda_wsgi
 from app import app
+import json
 
 def lambda_handler(event, context):
     # Ensure 'queryStringParameters' exists in the event
     if 'queryStringParameters' not in event:
         event['queryStringParameters'] = None
+
+    # Ensure 'httpMethod' exists in the event
+    if 'httpMethod' not in event:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'errorMessage': "KeyError: 'httpMethod' not found in event"})
+        }
+
     return aws_lambda_wsgi.response(app, event, context)
